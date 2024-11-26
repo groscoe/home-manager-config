bba5d0dfbff3cef89e34321f77f787c92a1cbbe5
GRoscoe <gustavo@gustavoroscoe.com>
2024-11-26 07:44:58 -0300

    Conda initialization

--- fish/fish.nix
+++ fish/fish.nix
@@ -52,7 +52,21 @@
       # Homebrew
       if [ -e /opt/homebrew/bin/brew ]
         eval "$(/opt/homebrew/bin/brew shellenv)"
+
+        # >>> conda initialize >>>
+        # !! Contents within this block are managed by 'conda init' !!
+        if test -f /opt/homebrew/Caskroom/miniconda/base/bin/conda
+            eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
+        else
+            if test -f "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
+                . "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
+            else
+                set -x PATH "/opt/homebrew/Caskroom/miniconda/base/bin" $PATH
+            end
+        end
+        # <<< conda initialize <<<
       end
+
     '';
 
     interactiveShellInit = ''
